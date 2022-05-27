c47400714add770120b4c2ea040a12169e963952
jamesliao <JamesCoding888@hotmail.com>
2022-05-27 21:52:18 +0800

    JavaWeb-20220418

---
+++ src/main/java/jdbc/CreateDBServlet.java
@@ -0,0 +1,43 @@
+package jdbc;
+
+import java.io.IOException; 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/jdbc/create/db")
+public class CreateDBServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 資料庫放置位置
+		String db_path = "C:/Users/jamesliao/eclipse-workspace-web/JavaWeb-20220418/db";
+		// 資料庫名稱
+		String db_name = "webdb.db";
+		// 建立資料庫連線參數
+		String db_url = "jdbc:sqlite:" + db_path + "/" + db_name;
+		// 資料庫連線物件
+		Connection conn = null;
+		try {
+			// 建立資料庫 driver
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection(db_url); // SQLite 沒有 account and password
+			resp.getWriter().print("Connection to SQLite has been established!");
+			
+		} catch (Exception e) {
+			resp.getWriter().print(e.getMessage()); // 有任何錯誤發生，拋出錯誤訊息
+		} finally {
+			if(conn != null) {
+				try {
+					conn.close();	
+				} catch (SQLException e) {
+					resp.getWriter().print(e.getMessage()); // 有任何錯誤發生，拋出錯誤訊息
+				}
+			}
+		}			
+	}	
+}
---
+++ src/main/java/jdbc/User.java
@@ -0,0 +1,45 @@
+package jdbc;
+
+import java.util.Date;
+
+public class User {
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Date createtime;
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", createtime=" + createtime + "]";
+	}
+	
+}
